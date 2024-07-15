a129a424352aebbb28ddac4060fd378c4d82195e
ItaiApiiro <90134061+ItaiApiiro@users.noreply.github.com>
2024-07-15 11:55:13 +0300

    Update AccountUserResponse.java

--- f2/PaymentData/AccountUserResponse.java
+++ f2/PaymentData/AccountUserResponse.java
@@ -3,8 +3,7 @@ public class AccountUserResponse{
 
     private String email;
     private String firstName;
-    private String lastName;
-    private String phoneNumber;
     private String userId;
-
+     private String generalId;
+     private String extraSecretStuff;
 }
